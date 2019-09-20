641328bb9063593a7664eff379852fc4a34c2e24
WangJie <wangmun92@gmail.com>
2019-09-21 02:45:29 +0800

    添加摇乳demo1-1.js

--- test/autoindent.js
+++ test/autoindent.js
@@ -1,6 +1,8 @@
-// 绘制切片
-gl.texture(
-    { x : 0, y : 0, width : 30, height : 30 });
+﻿var dasdsa;        
 
-gl.end();
-
+var maxFrame = 0;
+for(var i = 0; i < this.items.length; i++) {
+    if(this.items[i].maxFrame > maxFrame) {
+        maxFrame = this.items[i].maxFrame;
+    }
+}
