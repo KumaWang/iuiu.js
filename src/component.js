b66de05f1c6297a3b0ca330a585efab74b9e9be3
WangJie <wangmun92@gmail.com>
2019-09-28 17:11:08 +0800

    111

--- src/component.js
+++ src/component.js
@@ -1,11 +1,12 @@
 function Component() {
-	this.components = {};
+    this.components = {};
 }
 
 Component.prototype.create = function(name) {
-	return this.components[name]();
+    return this.components[name]();
 }
 
 Component.prototype.define = function(name, createFunc) {
-	this.components[name] = createFunc;
+    this.components[name] = createFunc;
 }
+
